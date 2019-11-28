28a59b66d39a4f7cfa6aa134bc4a945d8048b143
ThomasPetrusVDC <46361901+ThomPetrus@users.noreply.github.com>
2019-11-28 10:02:46 -0800

    Update uploadServlet.java

--- 304_lab8/src/uploadServlet.java
+++ 304_lab8/src/uploadServlet.java
@@ -11,10 +11,14 @@ import javax.servlet.http.Part;
 @WebServlet("/uploadServlet")
 @MultipartConfig(maxFileSize = 16177215)
 public class uploadServlet extends HttpServlet {
-	//connection info
-	private String url = "jdbc:sqlserver://sql04.ok.ubc.ca:1433;DatabaseName=db_tvande;";
-	private String uid = "tvande";
-	private String pw = "33970138";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// connection info
+	String url = "jdbc:sqlserver://sql04.ok.ubc.ca:1433;DatabaseName=db_tvande;";
+	String uid = "tvande";
+	String pw = "33970138";
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
@@ -28,41 +32,55 @@ public class uploadServlet extends HttpServlet {
 		String idFromForm = request.getParameter("idForm");
 
 		if (idFromForm != null) {
-
+			// Parse the string from the form to an integer.
 			try {
 				id = Integer.parseInt(idFromForm);
 			} catch (Exception e) {
 				System.err.print("Could not convert product Id to integer.");
 			}
-
+			// Retrieve the file part 
 			Part filePart = request.getPart("image");
 
 			if (filePart != null) {
+				// Get input stream for the file
 				in = filePart.getInputStream();
 
-				try {
-					//Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				try {	
+	
+					// Make connection
+					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 					con = DriverManager.getConnection(url, uid, pw);
 
 					// First verify the productId entered is for a valid product in the database.
 					String SQL = "SELECT * FROM product WHERE productId =" + id;
 					PreparedStatement pstmt = con.prepareStatement(SQL);
 					ResultSet rst = pstmt.executeQuery();
+					
+					// If the resultSet returns a row the product exists in the relation
 					if (rst.next()) {
-						SQL = "UPDATE product SET productImage = ? WHERE productId = " + id + ";";
+						SQL = "UPDATE product SET productImage = ? WHERE productId = ? ";
 						pstmt = con.prepareStatement(SQL);
+						
+						// The following sets the input stream as a blob 
 						if (in != null)
 							pstmt.setBlob(1, in);
+						pstmt.setInt(2, id);
 						result = pstmt.executeUpdate();
 
+						// If result set returns 1 then success
 						if (result > 0) {
 							message = "<h1 align=\"center\">Image Succesfully Uploaded!</h1>";
 						} else {
-							message = "<h1 align=\"center\">Invalid Product Id!</h1>";
-						}
+							message = "<h1 align=\"center\">Image Upload Error.</h1>";
+						} 
+						
+					} else {
+						message = "<h1 align=\"center\">Invalid Product Id!</h1>";
 					}
 				} catch (SQLException ex) {
 					System.err.print(ex);
+					message = "Error: " + ex.getMessage();
+				}catch(Exception ex) {
 					message = "Error: " + ex.getMessage();
 				} finally {
 					if (con != null) {
@@ -73,7 +91,7 @@ public class uploadServlet extends HttpServlet {
 						}
 					}
 					request.setAttribute("Message", message);
-					getServletContext().getRequestDispatcher("/uploadMessage.jsp").forward(request, response);
+					getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
 				}
 			}
 		}
